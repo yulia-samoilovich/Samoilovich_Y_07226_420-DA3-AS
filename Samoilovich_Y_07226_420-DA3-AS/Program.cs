de10d31c84cf699f3371d0cba034e4c7683bbbe4
yulia <yulia@LAPTOP-QLQMKJ6R>
2022-09-01 14:35:18 -0400

    Add project files.

---
+++ Samoilovich_Y_07226_420-DA3-AS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Samoilovich_Y_07226_420_DA3_AS
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
